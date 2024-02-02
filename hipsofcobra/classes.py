b9b57b45a902fb8b355582d9c0667f77fb30b72f
blackstonep <blackstonep924@gmail.com>
2024-02-01 22:01:05 -0500

    working towards having G superlist ready to go

--- hipsofcobra/classes.py
+++ hipsofcobra/classes.py
@@ -131,57 +131,121 @@ def thetaK0(MeanQ=False):
   return 2*(DeltaK0(MeanQ=MeanQ)+GammaK0(MeanQ=MeanQ))
 
 class HipsofCobra():
-  def __init__(self, clist, method):
+  def __init__(self, clist, Pname, method):
+    _npi = 1
+    _nK  = np.sqrt(3.0)/2.0
+
     assert method=='derived' or method=='direct' , \
       " Method must be either 'derived' or 'direct'. "
+    assert Pname=='pi' or Pname=='K' , \
+      " Pname must be either 'pi' or 'K'. "
 
     self.clist  = clist
     self.method = method 
-    _l85   = rng.normal( Params.L85 , Params.L85sig  )
-    _l64   = rng.normal( Params.L64 , Params.L64sig  )
-    _mpi   = rng.normal( Params.mpi , Params.mpisig  )
-    _mk    = rng.normal( Params.mk  , Params.mksig   )
-    _meta  = rng.normal( Params.meta, Params.metasig )
-    _F0    = rng.normal( Params.F0  , Params.F0sig   )
-    xi_hat = self.clist[0]
-    xi_s   = self.clist[1]
-    xi_g   = self.clist[2] * Params.alpha**2 / (3.0 * np.pi * Params.beta) 
+    self.xi_hat = self.clist[0]
+    self.xi_s   = self.clist[1]
+    self.xi_g   = self.clist[2]*Params.alpha**2/(3.0*np.pi*Params.beta) 
+    Gpi_deriv_mean =  self.xi_g 
+    Gpi_deriv_std  =  0.0191309 # Unc. from (mpi/4*pi*F_pi)^2
+    GK_deriv_mean  = -0.536731
+    GK_deriv_std   =  0.239351 # Unc. from (mK/4*pi*F_pi)^2
+
+    # Extract mean and stdev of Gpi0 and GK0 values. 
+    _gsamples = 1000
+    _gpilist  = [self.Gpi0() for i in range(_gsamples)]
+    _gKlist   = [self.GK0()  for i in range(_gsamples)]
+    gpi0mean = np.average(_gpilist)
+    gpi0std  = np.std(_gpilist)
+    gK0mean  = np.average(_gKlist)
+    gK0std   = np.std(_gKlist)
     
-    if self.method=='derived':
-      # Read in constituent form factors in order to derive 
-      #   G form fractors. 
-      with open('input/hips_gammapi.txt', 'r') as file:
-        self.gammapi_sl = eval(file.read().replace('C', 'c') )
-      with open('input/hips_deltapi.txt', 'r') as file:
-        self.deltapi_sl = eval(file.read().replace('C', 'c') )
-      with open('input/hips_thetapi.txt', 'r') as file:
-        self.thetapi_sl = eval(file.read().replace('C', 'c') )
-      with open('input/hips_gammaK.txt', 'r') as file:
-        self.gammaK_sl = eval(file.read().replace('C', 'c') )
-      with open('input/hips_deltaK.txt', 'r') as file:
-        self.deltaK_sl = eval(file.read().replace('C', 'c') )
-      with open('input/hips_thetaK.txt', 'r') as file:
-        self.thetaK_sl = eval(file.read().replace('C', 'c') )
+    # Read in C and D functions (canonical Omnes Solutions)
+    #   in order to directly compute G form factors. 
+    with open('input/hips_c1.txt', 'r') as file:
+      c1_sl = eval(file.read().replace('C', 'c') )
+    with open('input/hips_c2.txt', 'r') as file:
+      c2_sl = eval(file.read().replace('C', 'c') )
+    with open('input/hips_d1.txt', 'r') as file:
+      d1_sl = eval(file.read().replace('C', 'c') )
+    with open('input/hips_d2.txt', 'r') as file:
+      d2_sl = eval(file.read().replace('C', 'c') )
 
-    else:
-      # Read in C and D functions (canonical Omnes Solutions)
-      #   in order to directly compute G form factors. 
-      with open('input/hips_c1.txt', 'r') as file:
-        c1_sl = eval(file.read().replace('C', 'c') )
-      with open('input/hips_c2.txt', 'r') as file:
-        c2_sl = eval(file.read().replace('C', 'c') )
-      with open('input/hips_d1.txt', 'r') as file:
-        d1_sl = eval(file.read().replace('C', 'c') )
-      with open('input/hips_d2.txt', 'r') as file:
-        d2_sl = eval(file.read().replace('C', 'c') )
+    slist = c1_sl[0]
+    number_of_inds  = len(c1_sl[0]) 
+    number_of_iters = len(c1_sl[1])
+    # Compute C and D derivatives
+    c1_deriv = [ 0.5*( 
+      (c1_sl[1][iter][1]-c1_sl[1][iter][0]) / (slist[1]-slist[0]) + \
+      (c1_sl[1][iter][2]-c1_sl[1][iter][1]) / (slist[2]-slist[1])
+    ) for iter in range(number_of_iters) ]
+    c2_deriv = [ 0.5*( 
+      (c2_sl[1][iter][1]-c2_sl[1][iter][0]) / (slist[1]-slist[0]) + \
+      (c2_sl[1][iter][2]-c2_sl[1][iter][1]) / (slist[2]-slist[1])
+    ) for iter in range(number_of_iters) ] 
+    d1_deriv = [ 0.5*( 
+      (d1_sl[1][iter][1]-d1_sl[1][iter][0]) / (slist[1]-slist[0]) + \
+      (d1_sl[1][iter][2]-d1_sl[1][iter][1]) / (slist[2]-slist[1])
+    ) for iter in range(number_of_iters) ] 
+    d2_deriv = [ 0.5*( 
+      (d2_sl[1][iter][1]-d2_sl[1][iter][0]) / (slist[1]-slist[0]) + \
+      (d2_sl[1][iter][2]-d2_sl[1][iter][1]) / (slist[2]-slist[1])
+    ) for iter in range(number_of_iters) ]
+    
+    self.G_sl = [slist, []]   
+    for iter in range(number_of_iters):
+      thetapi0_dummy = thetaPi0()
+      thetaK0_dummy = thetaK0()
+      Gpi0_dummy = self.Gpi0()
+      GK0_dummy  = self.GK0()
+      Gpi_deriv_dummy = rng.normal( Gpi_deriv_mean, Gpi_deriv_std )
+      GK_deriv_dummy  = rng.normal( GK_deriv_mean, GK_deriv_std )
 
-  
+      Qpi0 = _npi*Gpi0_dummy
+      QK0  = _nK*GK0_dummy
+      if self.method=='derived':
+        Qpi1 = _npi*Gpi_deriv_dummy-\
+                  self.xi_g*(
+                    _npi*thetapi0_dummy*c1_deriv[iter]+\
+                    _nK*thetaK0_dummy*d1_deriv[iter]          
+                  )
+        QK1  = _nK*GK_deriv_dummy-\
+                  self.xi_g*(
+                    _nK*thetaK0_dummy*d2_deriv[iter]+\
+                    _npi*thetapi0_dummy*c2_deriv[iter]
+                  )
+      else: 
+        Qpi1 = _npi*Gpi_deriv_dummy-\
+                c1_deriv[iter]*Gpi0_dummy-\
+                d1_deriv[iter]*GK0_dummy
+        QK1  = _nK*GK_deriv_dummy-\
+                c2_deriv[iter]*Gpi0_dummy-\
+                d2_deriv[iter]*GK0_dummy
+      
+      if Pname=='pi':
+        gvals = [
+          c1_sl[1][iter][i]*(Qpi0+slist[i]*Qpi1) +\
+          d1_sl[1][iter][i]*(QK0+slist[i]*QK1)/_npi
+          for i in range(number_of_inds)]
+      else: 
+        gvals = [
+          c2_sl[1][iter][i]*(Qpi0+slist[i]*Qpi1) +\
+          d2_sl[1][iter][i]*(QK0+slist[i]*QK1)/_nK
+          for i in range(number_of_inds)]
+      self.G_sl[1].append(gvals)
 
-a = HipsofCobra([1,1,1], 'derived')  
-ic( a.clist ) 
-ic( GammaPi0() )
-ic( DeltaPi0() )
-ic( GammaK0() )
-ic( DeltaK0() )
-ic( thetaPi0() )
-ic( thetaK0() )
+
+    
+
+  def Gpi0(self, MeanQ=False):
+    return self.xi_g*thetaPi0(MeanQ=MeanQ)-\
+      (self.xi_hat+(1.0-Params.gamma)*self.xi_g)*GammaPi0(MeanQ=MeanQ)-\
+      (self.xi_s+(1.0-Params.gamma)*self.xi_g)*DeltaPi0(MeanQ=MeanQ)
+
+  def GK0(self, MeanQ=False):
+    return self.xi_g*thetaK0(MeanQ=MeanQ)-\
+      (self.xi_hat+(1.0-Params.gamma)*self.xi_g)*GammaK0(MeanQ=MeanQ)-\
+      (self.xi_s+(1.0-Params.gamma)*self.xi_g)*DeltaK0(MeanQ=MeanQ)
+
+
+a = HipsofCobra([1,1,1], 'pi', 'derived')  
+ic( a.G_sl[1][1][:10] ) 
